e718da0ceba8be07f011b0db162f35a2fa16d273
Kate <akinyi180@gmail.com>
2019-11-11 16:47:20 +0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,9 @@
+
+from django.shortcuts import render
+from django.views.generic import ListView
+from core.models import Movie
+# Create your views here.
+
+
+class MovieList(ListView):
+    model = Movie
